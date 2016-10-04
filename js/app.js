5e8165704a2b5a97ae81a053988e80223b1e9055
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-04 16:34:55 -0400

    APP: created new function to generate array with 0s taking two arguments for rows and columns

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //DOM event listener
 $(function() {
   console.log('DOM loaded and ready!');
-  populateArray(gameArray);
+  // populateArray(gameArray);
+  createArray(3,3);
 })
 
 //size of the board, will be taken from get form
@@ -9,6 +10,7 @@ var boardSize = 3;
 
 //creating the vertical array to nest horizontal arrays
 var gameArray = new Array([],[],[]);
+var gameArray2 = [];
 
 // default function that populates a 3x3 array
 //with values from 1 to 9
@@ -21,6 +23,23 @@ function populateArray(arrr) {
       console.log(`row ${i}, column ${j} = ${arrr[i][j]}`);
     }
   }
+}
+createArray(3,3);
+
+//FUNCTION: creates an empty array by taking arguments of
+//horizontal size and vertical size
+function createArray(x, y) {
+  let gameArray3 = [];
+  //creating a for loop to give columns
+  for (let i = 0; i < x; i++) {
+    //using push method to add array to the array
+    gameArray3.push([]);
+    for (let j = 0; j < y; j++) {
+      //unsing push method to populate values
+      gameArray3[i].push('0')
+    }
+  }
+  console.log(gameArray3);
 }
 
 
