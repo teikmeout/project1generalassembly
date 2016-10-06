3962c6d991f4ad56bc259eaa428dc107a0c27081
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-06 12:31:22 -0400

    added function to count how many mines are around each array value

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 //DOM event listener
-$(function() {
-  console.log('DOM loaded and ready!');
-  // populateArray(gameArray);
-  createArray(3,3);
-})
+// $(function() {
+//   console.log('DOM loaded and ready!');
+//   // populateArray(gameArray);
+//   createArray(3,3);
+// })
 
 //size of the board, will be taken from get form
 var boardSize = 3;
@@ -29,8 +29,8 @@ var gameArray = new Array(row1,row2,row3,row4);
 //   }
 // }
 
-//FUNCTION: creates an empty array by taking arguments of
-//horizontal size and vertical size
+//FUNCTION: creates an empty array by taking
+//ARGUMENTS: horizontal size and vertical size
 function createArray(x, y) {
   let gameArray3 = [];
   //creating a for loop to give columns
@@ -45,23 +45,117 @@ function createArray(x, y) {
   console.log(gameArray3);
 }
 
-//FUNCTION: one by one check how many mines are around
-function flagger() {
+//FUNCTION: one by one console where the mines are.
+//ARGUMENTS: the array to be checked
+function flagger(arrr) {
   //double for loop for iteration in matrix
-  for (let i = 0; i < gameArray.length; i++) {
-    for (let j = 0; j < gameArray[i].length; j++) {
+  for (let i = 0; i < arrr.length; i++) {
+    for (let j = 0; j < arrr[i].length; j++) {
         //if there's a -1 display that you have hit a mine
-        if (gameArray[i][j] == -1) {
+        if (arrr[i][j] == -1) {
           console.log(`hit a mine at ${i},${j}`);
+          addOneAllAround(arrr);
         } else console.log(`NO MINE at ${i},${j}`);
-
     }
   }
 }
 flagger(gameArray);
 
-//function created to add +1 to boxes around when you hit a mine
-function addOneAllAround(arrr, hor, ver) {
-
+//function created to count how many mines are around the boxes around
+//ARGUMENTS: the array to check, values or i and j from a for loop
+function addOneAllAround(arrr) {
+  for (let i = 0 ; i < arrr.length; i++) {
+    for (let j = 0; j < arrr[i].length; j++) {
+      //temporary counter to accumulate mines around
+      let counter = 0;
+      //IF that only checks when left border is not 0
+      if (j > 0) {
+        if (arrr[i][j-1] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks right when i != boardSize -1
+      if (j < arrr[i].length-1) {
+        if (arrr[i][j+1] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks top if not in arrr[0]
+      if (i > 0) {
+        if (arrr[i-1][j] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks bottom if not at the bottom border
+      if (i < arrr.length-1) {
+        if (arrr[i+1][j] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks when not at topleft corner
+      if (i > 0 && j > 0 ) {
+        if (arrr[i-1][j-1] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks when not at bottom left corner
+      if (i < arrr.length-1 && j > 0) {
+        if (arrr[i+1][j-1] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks when not at topright corner
+      if (i > 0 && j < arrr[i].length-1) {
+        if (arrr[i-1][j+1] == -1) {
+          counter++;
+        }
+      }
+      //IF that only checks when not at bottom right corner
+      if (i < arrr.length-1 && j < arrr[i].length-1) {
+        if (arrr[i+1][j+1] == -1) {
+          counter++;
+        }
+      }
+      if (arrr[i][j] !== -1) {
+        arrr[i][j] = counter;
+      }
+    }
+  }
 }
+
+console.log(gameArray);
+
+  // //THIS PART ONLY APPLIES WHEN AT THE CENTER
+  // //if left element is not -1 ADD 1 we are NOT at the left border
+  // if (hor != 0 && arrr[hor-1][ver] != -1){
+  //   arrr[hor-1][ver]++;
+  // }
+  // //if top left element is not -1 ADD 1 and we are NOT at the left or top border
+  // if (hor != 0 && vert != 0 && arrr[hor-1][ver-1] != -1){
+  //   arrr[hor-1][ver-1]++;
+  // }
+  // //if bottom left element is not -1 ADD 1 and we are not at the left or bottom border
+  // if (hor != 0 && && arrr[hor-1][ver+1] != -1) {
+  //   arrr[hor-1][ver-1]++;
+  // }
+  // //if top element is not -1 ADD 1
+  // if (arrr[hor][ver+1] != -1) {
+  //   arrr[hor][ver]++;
+  // }
+  // //if bottom element is not -1 ADD 1
+  // if (arrr[hor][ver-1] != -1) {
+  //   arrr[hor][ver-1]++;
+  // }
+  // //if right element is not -1 ADD 1
+  // if (arrr[hor+1][ver] != -1) {
+  //   arrr[hor+1][ver]++;
+  // }
+  // //if top right element is not -1 ADD 1
+  // if (arrr[hor+1][ver+1] != -1) {
+  //   arrr[hor+1][ver+1]++;
+  // }
+  // //if bottom right element is not -1 ADD 1
+  // if (arrr[hor+1][ver-1] != -1) {
+  //   arrr[hor+1][ver-1]++;
+
 
